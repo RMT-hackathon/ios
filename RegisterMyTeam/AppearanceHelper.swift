e2dd50eda316fc7f6a9d6d566d3b2f6c630b29b2
Kobe McKee <kobe.mckee@gmail.com>
2020-01-02 09:54:03 -0700

    Initial Commit

---
+++ RegisterMyTeam/AppearanceHelper.swift
@@ -0,0 +1,26 @@
+//
+//  AppearanceHelper.swift
+//  RegisterMyTeam
+//
+//  Created by Kobe McKee on 1/2/20.
+//  Copyright © 2020 Kobe McKee. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum AppearanceHelper {
+    
+    static let brandBlue = UIColor(hue: 237/255, saturation: 99/255, brightness: 32/255, alpha: 1.0)
+    
+    static let textBlack = UIColor(hue: 0/255, saturation: 0/255, brightness: 20/255, alpha: 1.0)
+    
+    static let blueWhite = UIColor(hue: 191/255, saturation: 5/255, brightness: 94/255, alpha: 1.0)
+    
+    static let gray = UIColor(hue: 0/255, saturation: 0/255, brightness: 83/255, alpha: 1.0)
+    
+    static let white = UIColor(named: "white")
+    
+    
+    
+}
